8537f7a2738b01dd680ca6113bfd28f5da581143
TajievOlimjon <tajiev0711@gmail.com>
2022-10-21 22:38:10 +0500

    Add cw 3

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
