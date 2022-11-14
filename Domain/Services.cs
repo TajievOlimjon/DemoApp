4ab1fa33b989b03411453d0bf167b5a142df9cfb
Olimjon <tajiev0711@gmail.com>
2022-11-14 23:24:40 +0300

    chenges hellogit

--- Domain/Services.cs
+++ Domain/Services.cs
@@ -2,7 +2,6 @@
 {
     public class Book
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string Lastname { get; set; }
         public int Age { get; set; }
