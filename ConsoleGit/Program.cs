0ca07645ad31167d5065160416cdd2c7576cb25a
Olimjon <tajiev0711@gmail.com>
2022-11-14 23:20:59 +0300

    chenges book properties

--- ConsoleGit/Program.cs
+++ ConsoleGit/Program.cs
@@ -1,3 +1,6 @@
 ﻿using Domain;
 
 Book book = new Book();
+book.Age = 12;
+book.FirstName = "knkjn";
+ 
--- Domain/Services.cs
+++ Domain/Services.cs
@@ -3,7 +3,9 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
     }
 }
